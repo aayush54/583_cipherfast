7ad68ad2660a9351d6c483bdfae878eb41cf06fd
Taeyoon Kim <taeyoon@ggg.eecs.umich.edu>
2023-04-23 18:20:30 -0400

    randpass initial merge

--- project/mypass/mypass3.cpp
+++ project/mypass/mypass3.cpp
@@ -1,10 +1,19 @@
 #include "llvm/Pass.h"
+
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Value.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/GlobalVariable.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+
+#include "llvm/ADT/Statistic.h"
+#include "llvm/Support/raw_ostream.h"
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -12,18 +21,136 @@
 #include <unordered_map>
 #include <stddef.h>
 
+
+
 using namespace llvm;
+
+Function *main_func;
+std::unordered_map<int, Function *> maskGen;
+GlobalVariable *seed;
+
+// Create mask function for given size (8, 32, 64, etc.)
+Function *createXorshift(Module *module, int bitsize)
+{
+    LLVMContext &context = module->getContext();
+    // Type *int32Ty = Type::getInt32Ty(context);
+    Type *int64Ty = Type::getInt64Ty(context);
+    // Type *int32PtrTy = Type::getInt32PtrTy(context);
+    // Type *int64PtrTy = Type::getInt64PtrTy(context);
+
+    Type *intTy = Type::getIntNTy(context, bitsize);
+    Type *intPtrTy = Type::getIntNPtrTy(context, bitsize);
+
+    // Define the function type
+    FunctionType *funcType = FunctionType::get(intTy, intPtrTy, false);
+
+    // Create the function
+    std::string name = "__xorshift" + std::to_string(bitsize);
+    Function *func = Function::Create(
+        funcType, Function::LinkageTypes::PrivateLinkage,
+        name, module);
+
+    // Create the entry basic block
+    BasicBlock *entryBB = BasicBlock::Create(context, "entry", func);
+
+    // Create an IR builder
+    IRBuilder<> builder(entryBB);
+
+    // Allocate memory for the state pointer and the x variable
+    AllocaInst *stateAddr = builder.CreateAlloca(intPtrTy);
+    AllocaInst *x = builder.CreateAlloca(intTy);
+
+    // Store the state argument into the state pointer
+    Argument &stateArg = *(func->arg_begin());
+    builder.CreateStore(&stateArg, stateAddr);
+
+    // Load the state pointer and get the first element of the state array
+    LoadInst *stateLoad = builder.CreateLoad(intPtrTy, stateAddr);
+    Constant *zero = ConstantInt::get(int64Ty, 0);
+    Value *arrayIdx = builder.CreateGEP(intTy, stateLoad, zero);
+
+    // Load the first element of the state array into x
+    LoadInst *xLoad = builder.CreateLoad(intTy, arrayIdx);
+    builder.CreateStore(xLoad, x);
+
+    // Apply the xorshift32 algorithm to x
+    Value *shl = builder.CreateShl(xLoad, 13);
+    Value *xor1 = builder.CreateXor(xLoad, shl);
+    builder.CreateStore(xor1, x);
+
+    Value *shr = builder.CreateLShr(xor1, 17);
+    Value *xor2 = builder.CreateXor(xor1, shr);
+    builder.CreateStore(xor2, x);
+
+    Value *shl2 = builder.CreateShl(xor2, 5);
+    Value *xor3 = builder.CreateXor(xor2, shl2);
+    builder.CreateStore(xor3, x);
+
+    // Store the updated x back into the state array
+    LoadInst *xFinalLoad = builder.CreateLoad(intTy, x);
+    builder.CreateStore(xFinalLoad, arrayIdx);
+
+    // Return the final value of x
+    builder.CreateRet(xFinalLoad);
+    return func;
+}
 
 namespace {
   struct PrintMemoryWrites : public FunctionPass {
     static char ID;
     PrintMemoryWrites() : FunctionPass(ID) {}
 
+      bool doInitialization(Module &M) override
+        {
+            maskGen[8] = createXorshift(&M, 8);
+            maskGen[16] = createXorshift(&M, 16);
+            maskGen[32] = createXorshift(&M, 32);
+            maskGen[64] = createXorshift(&M, 64);
+            errs() << "Created func\n";
+
+            // Get the LLVM context
+            LLVMContext &context = M.getContext();
+            auto intType = IntegerType::get(context, 64);
+
+            // Create an array type with one element of type i32
+
+            std::vector<Constant *> values;
+            values.push_back(ConstantInt::get(intType, 47));
+            auto arrType = ArrayType::get(intType, values.size());
+
+            auto init = ConstantArray::get(arrType, values);
+
+            seed = (llvm::GlobalVariable *)M.getOrInsertGlobal("seed", arrType);
+            seed->setInitializer(init);
+            seed->setConstant(false);
+            seed->setLinkage(llvm::GlobalValue::LinkageTypes::PrivateLinkage);
+            seed->setUnnamedAddr(llvm::GlobalValue::UnnamedAddr::Global);
+
+            errs() << "Created global\n";
+
+            // Add the global variable to the module
+            return true;
+        }
+
+
     // create the hash table in the global space
     std::unordered_map< Value* /*memory address*/, Value* /*mask address*/> m;
     unsigned int start_address = 0;
 
     bool runOnFunction(Function &F) override {
+
+            //! Make sure not to instrument the xorshift mask generators
+            if (F.getName().substr(0, 10) == "__xorshift")
+                return false;
+
+            errs() << F.getName() << "\n" << "\n";
+
+            Instruction &x = F.front().front();
+            IRBuilder<> builder(&x);
+            builder.CreateCall(maskGen[32], seed);
+            std::cout<< "IR Builder" << x << "\n";
+
+
       std::srand(std::time(nullptr)); // initialize random seed
       LLVMContext &Context = F.getContext();
       for (auto &BB : F) {
@@ -130,9 +257,11 @@ namespace {
           //   // (value stored in the load mem address) XOR (mask) =
              
           // }
+        
+      
         }
       }
-      return false;
+      return true;
     }
   };
 }
